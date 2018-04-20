287913ec1b49bb4157b89144deff6166809235b6
linyi <linyi_ios@icloud.com>
2018-04-20 15:56:32 +0800

    增加 SocketRequest 实现
    这个设计模式有点好玩儿，中间件的意思。

--- LYSocketDomo/LYSocket/SocketRequest.m
+++ LYSocketDomo/LYSocket/SocketRequest.m
@@ -7,9 +7,8 @@
 //
 
 #import "SocketRequest.h"
-
-
-typedef void (^kCallBackBlock)(id);
+#import "TCPSocket.h"
+typedef void (^callBackBlock)(id);
 
 static NSString *APP_PARAM = @"smart";
 static NSString *CO_PARAM = @"kjd";
@@ -17,10 +16,151 @@ static NSMutableDictionary *_successBlockDic;
 static NSMutableDictionary *_failureBlockDic;
 static NSTimeInterval requestTimeOut = 10;
 
-// const 关键字的作用，组合的作用。
+// const 关键字的作用，组合的作用是啥？
 static NSString *const ERROR_NOTI_NAME = @"ly_error_noti";
-static TCPSocket *tcpScoket = nil;
+static TCPSocket *_tcpScoket = nil;
 
 @implementation SocketRequest
+
++ (void)initialize{
+    _successBlockDic = [NSMutableDictionary dictionary];
+    _failureBlockDic = [NSMutableDictionary dictionary];
+}
+
++ (void)socket:(TCPSocket *)tcpSocket apiName:(NSString *)apiName parameters:(id)parameters success:(void (^)(id))success failure:(void (^)(NSError *))failure{
+    
+    if(!_tcpScoket){
+        NSAssert(!_tcpScoket, @"请先调用(-setTcpSocket:appParam:co_param:)设置socket相关参数");
+        return;
+    }
+    [self socket:_tcpScoket apiName:apiName parameters:parameters success:success failure:failure];
+}
+
++ (void)socketWithApiName:(NSString *)apiName
+               parameters:(id)parameters
+                  success:(void (^)(id))success
+                  failure:(void (^)(NSError *))failure{
+    NSString *noti_name = [NSString stringWithFormat:@"ly_%@",apiName];
+    
+    //
+    if(success){
+        _successBlockDic[noti_name] = success;
+    }
+    if(failure){
+        _failureBlockDic[noti_name] = failure;
+        _failureBlockDic[ERROR_NOTI_NAME] = failure;
+    }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(get_result_noti:) name:noti_name object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(get_result_noti:) name:ERROR_NOTI_NAME object:nil];
+    
+    // 发送请求
+    [_tcpScoket sendDataToSocket:[self formatCmdWithApiName:apiName parameters:parameters]];
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(requestTimeOut * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        if(!_failureBlockDic[noti_name]){
+            return ;
+        }
+        
+        NSNotification *noti = [NSNotification notificationWithName:noti_name object:nil userInfo:@{@"-1" : @"request time out"}];
+        
+        
+//        [self get_result_noti:noti];
+        
+        [self get_result_noti:noti];
+    });
+}
+
+#pragma mark - 通知
+
++(void)get_result_noti:(NSNotification *)noti{
+    // 移除通知
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:noti.name object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self name:ERROR_NOTI_NAME object:nil];
+    
+    if (!noti.userInfo || noti.userInfo.count == 0) {
+        return;
+    }
+    
+    // 取出 retcode
+    NSInteger retcode = [[noti.userInfo allKeys][0] integerValue];
+    
+    if(retcode == 0){
+        callBackBlock success = _successBlockDic[noti.name];
+        [self removeBlockWithKey:noti.name];
+        if(success){
+            success(noti.object);
+        }
+    }else{
+        callBackBlock failure = _failureBlockDic[noti.name];
+        [self removeBlockWithKey:noti.name];
+        if(failure){
+            NSError *error = [NSError errorWithDomain:NSCocoaErrorDomain code:retcode userInfo:@{@"error":[noti.userInfo allValues]}];
+            failure(error);
+        }
+    }
+}
+
+/**
+ *  移除block
+ *
+ *  @param key key
+ */
++ (void)removeBlockWithKey:(NSString *)key{
+    
+    [_successBlockDic removeObjectForKey:key];
+    [_failureBlockDic removeObjectForKey:key];
+    [_failureBlockDic removeObjectForKey:ERROR_NOTI_NAME];
+}
+
+
+#pragma mark - help
+/**
+ *  格式化指令
+ */
++ (NSString *)formatCmdWithApiName:(NSString *)apiName parameters:(id)parameters{
+    
+    NSError *error = nil;
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:parameters options:NSJSONWritingPrettyPrinted error:&error];
+    
+    if (error) return nil;
+    
+    NSString *param = [[[[[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding] stringByReplacingOccurrencesOfString:@"\n" withString:@""] stringByReplacingOccurrencesOfString:@"  \"" withString:@"\""] stringByReplacingOccurrencesOfString:@" : " withString:@":"];
+    
+    return [NSString stringWithFormat:@"{\"app\":\"%@\",\"co\":\"%@\",\"api\":\"%@\",\"data\":%@}\n", APP_PARAM, CO_PARAM, apiName, param];
+}
+
+/**
+ *  参数设置
+ *
+ *  @param tcpSocket tcpSocket
+ *  @param app_param app_param
+ *  @param co_param  co_param
+ */
++ (void)setTcpSocket:(TCPSocket *)tcpSocket appParam:(NSString *)app_param co_param:(NSString *)co_param{
+    
+    if (tcpSocket) {
+        _tcpScoket = tcpSocket;
+    }
+    if (app_param) {
+        APP_PARAM = app_param;
+    }
+    if (co_param) {
+        CO_PARAM = co_param;
+    }
+}
+
+
+/**
+ *  设置请求超时时间
+ *
+ *  @param timeOut 超时时间
+ */
++ (void)setSocketRequsetTimeOut:(NSTimeInterval)timeOut{
+    
+    requestTimeOut = timeOut;
+    
+}
 
 @end
